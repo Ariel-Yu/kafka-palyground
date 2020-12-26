66b17739ddb4e9b1a01158b3e9ea96bc84fc3f6e
Ariel Yu <eyu@wayfair.com>
2020-12-26 16:07:53 +0100

    Add new entry point key_partition_produce

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import click
 
 from kafka_playground.domain.services.consume_service import ConsumeService
+from kafka_playground.domain.services.key_partition_produce_service import KeyPartitionProduceService
 from kafka_playground.domain.services.multi_consumer_groups_produce_service import MultiConsumerGroupsProduceService
 from kafka_playground.infrastructure.factories.consumer_factory import create_consumer
 from kafka_playground.infrastructure.factories.producer_factories import create_producer
@@ -26,8 +27,18 @@ def multi_consumer_groups_produce(topic: str):
 
 @cli.command()
 @click.argument('topic')
+def key_partition_produce(topic: str):
+    producer = create_producer()
+    service = KeyPartitionProduceService(producer)
+
+    click.echo("##### Start to produce message")
+    service.produce(topic)
+
+
+@cli.command()
+@click.argument('topic')
 @click.argument('consumer_group_name')
-def multi_consumer_groups_consume(topic: str, consumer_group_name: str):
+def consume(topic: str, consumer_group_name: str):
     consumer = create_consumer(consumer_group_name)
     service = ConsumeService(consumer)
 
