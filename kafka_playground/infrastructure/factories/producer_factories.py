2eb70f1b0055908c7a4df1eaf4753fa1f312dfcf
Ariel Yu <eyu@wayfair.com>
2020-12-27 14:41:17 +0100

    Add key_schema to avro producer

--- kafka_playground/infrastructure/factories/producer_factories.py
+++ kafka_playground/infrastructure/factories/producer_factories.py
@@ -1,16 +1,18 @@
+from typing import Optional
+
 from kafka_playground.infrastructure.producers.avro_producer import AvroProducer
 from kafka_playground.infrastructure.producers.producer import Producer
 from kafka_playground.settings import kafka_broker, schema_registry_broker
 
 
-def create_avro_producer(schema: str):
+def create_avro_producer(value_schema: str, key_schema: Optional[str] = None):
     config = {
         "bootstrap.servers": kafka_broker,
         "schema.registry.url": schema_registry_broker,
         "default.topic.config": {"acks": 1},
     }
 
-    return AvroProducer(config, schema)
+    return AvroProducer(config=config, value_schema=value_schema, key_schema=key_schema)
 
 
 def create_producer():
--- kafka_playground/infrastructure/producers/avro_producer.py
+++ kafka_playground/infrastructure/producers/avro_producer.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer as ConfluentAvroProducer
@@ -8,12 +9,12 @@ _FLUSH_TIMEOUT_SECONDS = 60
 
 
 class AvroProducer:
-    def __init__(self, config: dict, schema: str):
-        schema = avro.load(schema)
-        self._producer = ConfluentAvroProducer(config, default_value_schema=schema)
+    def __init__(self, config: dict, value_schema: str, key_schema: Optional[str] = None):
+        schema = avro.load(value_schema)
+        self._producer = ConfluentAvroProducer(config, default_value_schema=schema, default_key_schema=key_schema)
 
-    def produce(self, topic: str, value: str):
-        self._producer.produce(topic=topic, value=json.loads(value))
+    def produce(self, topic: str, value: str, key: Optional[str] = None):
+        self._producer.produce(topic=topic, value=json.loads(value), key=key)
         self._producer.poll(_POLL_TIMEOUT_SECONDS)
 
     def flush(self):
