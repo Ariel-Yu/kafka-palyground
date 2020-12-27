38db78812b0421aefbfe7a650084db2ab31df0cc
Ariel Yu <eyu@wayfair.com>
2020-12-27 16:12:59 +0100

    Add create_avro_consumer

--- kafka_playground/infrastructure/factories/consumer_factories.py
+++ kafka_playground/infrastructure/factories/consumer_factories.py
@@ -1,4 +1,5 @@
 from confluent_kafka import Consumer
+from confluent_kafka.avro import AvroConsumer
 
 from kafka_playground.settings import kafka_broker
 
@@ -15,3 +16,17 @@ def create_consumer(consumer_group_name: str):
     }
 
     return Consumer(config)
+
+
+def create_avro_consumer(consumer_group_name: str):
+    config = {
+        'bootstrap.servers': kafka_broker,
+        'group.id': consumer_group_name,
+        'max.poll.interval.ms': 6000,
+        'session.timeout.ms': 6000,
+        'default.topic.config': {
+            'auto.offset.reset': 'earliest'
+        }
+    }
+
+    return AvroConsumer(config)
--- tests/unit/infrastructure/factories/test_consumer_factory.py
+++ tests/unit/infrastructure/factories/test_consumer_factory.py
@@ -1,6 +1,6 @@
 from confluent_kafka.cimpl import Consumer
 
-from kafka_playground.infrastructure.factories.consumer_factory import create_consumer
+from kafka_playground.infrastructure.factories.consumer_factories import create_consumer
 
 
 def test_create_consumer():
